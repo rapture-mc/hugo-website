aa986279dd54274934bf4fbf0fd456a079b32caa
Ben Harris <ben.r.harris@protonmail.com>
2025-06-02 10:17:28 +0930

    moving javascript to static dir

---
+++ static/js/typed.js
@@ -0,0 +1,13 @@
+var typed = new Typed("#typedText", {
+  strings: [
+      "{{ .Site.Title }}",
+      "linux technologies...",
+      "linux anonymity...",
+      "linux posts...",
+      "linux everything!"
+    ],
+    typeSpeed: 150,
+    backSpeed: 75,
+    smartBackspace: true,
+    loop: true
+})
